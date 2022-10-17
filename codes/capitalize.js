7968a992f857a8ccfa0613bea5d7b80f268060fc
parkeralexjm <parkeralexjm@hotmail.co.uk>
2022-10-17 18:07:05 +0100

    Created a capitalize function and tests

---
+++ codes/capitalize.js
@@ -0,0 +1,7 @@
+function capitalize(input) {
+  let lowerCase = input.toLowerCase()
+  lowerCase = lowerCase.charAt(0).toUpperCase() + lowerCase.slice(1);
+  return lowerCase
+}
+
+module.exports = capitalize;
